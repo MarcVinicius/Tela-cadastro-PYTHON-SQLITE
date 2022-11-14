83b417a9d7b8135bf771d6d8697448e70b6340c5
MarcVinicius <viniciusmarcos.01@hotmail.com>
2022-11-13 23:27:35 -0300

    alteracoes codigo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from tela_cadastrar import Ui_Form
 from tela_main import Ui_main_wIndow
+from tela_editar import Ui_tela_editar
 
 
 #classe tela principal
@@ -15,6 +16,12 @@ class main(QtWidgets.QApplication, Ui_main_wIndow):
 
 #classe tela cadastrar e editar
 class cadastrar(QtWidgets.QApplication, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setup(self)
+
+#classe tela editar
+class editar(QtWidgets.QApplication, Ui_tela_editar):
     def __init__(self):
         super().__init__()
         self.setup(self)
@@ -36,7 +43,13 @@ class chamarTelaFunc:
 
     def cad(self, form, ui):
         form.show()
+        Form.setWindowTitle("Cadastrar")
+        Form.setFixedSize(505, 475)
         #ui.nome_lnedit.setText("teste")
+
+    def edit(self, Form, ui):
+        Form.show()
+        Form.setWindowTitle("Manutenção Registro")
 
 #editando dados funcionario
 class inserirEditarFunc:
@@ -79,6 +92,12 @@ def chamar_tela_cadastro():
 
     ui.criar_btt.clicked.connect(chamar_tela_cad)
 
+def chamar_tela_editar():
+    def chamar_tela_edit():
+        chamarTelaFunc().edit(tela_editar, ui3)
+
+    ui.manutencao_btt.clicked.connect(chamar_tela_edit)
+
 def cadastrar():
     def cadastrar_func():
         nome = ui2.nome_lnedit.text()
@@ -98,14 +117,20 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     main_wIndow = QtWidgets.QWidget()
     Form = QtWidgets.QWidget()
+    tela_editar = QtWidgets.QWidget()
     #tela principal
     ui = Ui_main_wIndow()
     ui.setupUi(main_wIndow)
     #tela cadastro/edição
     ui2 = Ui_Form()
     ui2.setupUi(Form)
+    #tela edição
+    ui3 = Ui_tela_editar()
+    ui3.setupUi(tela_editar)
+    #mostrar tela principal
     main_wIndow.show()
     #funcoes
     chamar_tela_cadastro()
+    chamar_tela_editar()
     cadastrar()
     sys.exit(app.exec_())
